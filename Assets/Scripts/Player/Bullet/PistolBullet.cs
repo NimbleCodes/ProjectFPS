3a70e1219272eb1478d199d2b9a4b1bd2ab1b6b9
Nimblebody <hiroomanhihi@naver.com>
2023-06-14 22:49:05 +0900

    06.14
    1.Gun shooting in wrong direction fixed
    2.enemy dummy added

--- Assets/Scripts/Player/Bullet/PistolBullet.cs
+++ Assets/Scripts/Player/Bullet/PistolBullet.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PistolBullet : MonoBehaviour
-{
+{    
     Rigidbody _rig;
-    float _speed;
+    float _speed,_x,_y,_z;
     private void Start()
     {
         _rig = GetComponent<Rigidbody>();
     }
-    public void Init(float speed)
+    public void Init(float rotationX, float rotationY,float rotationZ ,float speed)
     {
         _speed = speed;
-        Destroy(gameObject, 2f);                    //»ý¼º 2ÃÊµÚ ÃÑ¾Ë ÆÄ±«
+        _x = rotationX;
+        _y = rotationY;
+        _z = rotationZ;
+        Destroy(gameObject, 2f);                    
     }
+
 
     private void Update()
     {
-        _rig.AddForce(Vector3.forward * _speed);
+        _rig.AddForce(new Vector3(_x,_y,_z).normalized * _speed);
     }
+    
 }
--- Assets/Scripts/Player/Controller/Shoot.cs
+++ Assets/Scripts/Player/Controller/Shoot.cs
@@ -8,15 +8,19 @@ public class Shoot : MonoBehaviour
     [SerializeField] GameObject _bulletPrefab;
     [SerializeField] Transform _camRotation;
     GameObject Bullet;
-    float x,y;
+    float x,y,z;
     void Update()
     {
         x = Mathf.Cos(_camRotation.rotation.x * Mathf.Deg2Rad);
         y = Mathf.Sin(_camRotation.rotation.y * Mathf.Deg2Rad);
+        z = _shootPoint.transform.rotation.z;
         if (Input.GetMouseButtonDown(0))
         { 
-            Bullet = Instantiate(_bulletPrefab,_shootPoint.position,_camRotation.rotation);
-            Bullet.GetComponent<Rigidbody>().AddForce(new Vector3(x,y,_shootPoint.position.z).normalized * 15);
+            Bullet = Instantiate(_bulletPrefab,_shootPoint.position,transform.rotation);
+            Bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 600);
+            Destroy(Bullet, 1f);
+            //Bullet.GetComponent<PistolBullet>().Init(x,y,z,20);
+
         }
     }
 }
