4cd62fc24b892a5b3f5193858e503063c7c7491d
NimbleCodes <134697896+NimbleCodes@users.noreply.github.com>
2023-08-17 12:11:01 +0900

    0817

---
+++ Assets/Scripts/Props/OpenDoor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] Animator RightDoor;
+    [SerializeField] Animator LeftDoor;
+
+    void OnTriggerEnter(Collider other)
+    {
+        RightDoor.SetBool("OnTrigger",true);
+        LeftDoor.SetBool("OnTrigger",true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        RightDoor.SetBool("OnTrigger",false);
+        LeftDoor.SetBool("OnTrigger",false);
+    }
+}
