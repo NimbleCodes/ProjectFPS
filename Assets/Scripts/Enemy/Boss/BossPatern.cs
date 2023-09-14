4f32a353e34c1226e364c9bcb2d64d795f4b2bf5
NimbleCodes <134697896+NimbleCodes@users.noreply.github.com>
2023-09-14 12:05:40 +0900

    0914
    1. AlphaScene dash fixed

--- Assets/Scripts/Enemy/Boss/BossPatern.cs
+++ Assets/Scripts/Enemy/Boss/BossPatern.cs
@@ -1,26 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Serialization;
 using UnityEngine;
 
 public class BossPatern : MonoBehaviour
 {
     [SerializeField] HealthManager _health;
-    
+    [SerializeField] GameObject[] weapons;
     Transform _player;
     BossPhase _phase = new BossPhase();
-    float _curHealth, _defaultHealth = 300;
+    float _curHealth, _defaultHealth = 300, _attackRate=0f;
+    bool _phase1Started=false, _phase2Started=false, _phase3Started=false;
     
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _health.SetHealth(_defaultHealth);
+
+        // set weapons active to false
+        for(int i =0; i < weapons.Length; i++){
+            weapons[i].SetActive(false);
+        }
+
     }
 
     void Update()
     {
         CheckCurHealth();
         PhaseChanger();
+        CheckPhase();
+
+        if(_phase1Started) Phase1();
+        if(_phase2Started) Phase1();
+        if(_phase3Started) Phase1();
+
     }
 
     void CheckCurHealth(){
@@ -32,33 +44,70 @@ public class BossPatern : MonoBehaviour
             _phase = BossPhase.Phase3;
         }else if(_curHealth <= 200){
             _phase = BossPhase.Phase2;
+        }else if(_curHealth <= 300){
+            _phase = BossPhase.Phase1;
         }
     }
 
-    void BossPhaseStart(){ //Phase1 production + set phase to pahse1
+    void CheckPhase(){
+        if(_phase == BossPhase.Phase1){
+            StartPhase1();
+        }else if(_phase == BossPhase.Phase2){
+            StartPhase2();
+        }else if(_phase == BossPhase.Phase3){
+            StartPhase3();
+        }
+    }
+
+    void StartPhase1(){
         // set default setting of the Boss
-        // show production of phase1
+        // set Phase1 weapon
+        ChangeWeapon(0);
+        _attackRate = 2f;
+
+
+        //set flag to ture
+        _phase1Started = true;
+
     }
 
     void Phase1(){
-        // set Phase1 weapon
-        // set attack rate
         // set movement patern of the boss
+        gameObject.transform.LookAt(_player);
+
         
     }
 
-    void StartPhase2(){ //Phase2 production
+    void StartPhase2(){
+        //set _phase1 flag to false, stop phase1 
+        _phase1Started = false;
+        ChangeWeapon(1);
+        _attackRate = 2.3f;
 
+        //set flag to true
+        _phase2Started = false;
     }
     void Phase2(){
 
     }
 
-    void StartPhse3(){ //Phase3 production
+    void StartPhase3(){
 
     }
     void Phase3(){
 
+    }
+
+    void Dash(){
+
+    }
+
+    void ChangeWeapon(int weaponNum){
+        for(int i =0; i < weapons.Length; i++){
+            weapons[i].SetActive(false);
+        }
+
+        weapons[weaponNum].SetActive(true);
     }
     
 }
