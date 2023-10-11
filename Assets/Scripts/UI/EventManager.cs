0ce512a90b7e35eba1eab541444f1bd252284eaf
NimbleCodes <134697896+NimbleCodes@users.noreply.github.com>
2023-10-11 10:30:41 +0900

    10.11
    ---- 추가사항 ----
    1. EventManager 추가 [작업중]
        - 필요시 현 코드 재설계
    2. Enemy Spawner 추가 (스폰 위치는 고정)
    3. BossPatern 추가(애니메이션 포함)
    4. Bullet 종류 추가(Particle)
        - 각 총기류에 맞게
    5. Sound 추가
        [intro + stage + boss + gameover + clear]
        [gunSound + Explosion(if time left) + Ammo added]
        [dash + weapon swap]
    6. 총 반동추가
    
    ---- ETC ----
    1. 맵 추가(기존맵 ++)

--- Assets/Scripts/UI/EventManager.cs
+++ Assets/Scripts/UI/EventManager.cs
@@ -1,6 +1,46 @@
+using System;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    private static EventManager instance = null;
+
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }else{
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static EventManager Instance{//singleton
+        get{
+            if(instance = null){
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+
+    // 스테이지 시작시 모든 데이터 init
+    // 모든 스포너 init
+    public event Action StageStartEvent;
+
+    // 스테이지 종료시 모든 데이터 초기화
+    // 모든 스포너 초기화
+    // 이미 나와있는 몬스터 초기화
+    // respawn point로 캐릭터 옮김
+    public event Action GameOverEvent;
     
+    // 모든 이펙트 볼륨 변경 함수를 등록
+    // 이펙트 value 변경시 event 실행, 모든 이팩트 볼륨 관리
+    public event Action<float> EffectsVolumeChangeEvent;
+    public event Action BossTriggerEvent;
+
+
 }
